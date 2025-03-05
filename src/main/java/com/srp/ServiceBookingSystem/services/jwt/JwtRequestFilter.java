18f073378c1f8651ce4958a0392f525bf32c060a
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-05 22:59:21 +0530

    Worked on JWT AUTHENTICATION Token

--- src/main/java/com/srp/ServiceBookingSystem/services/jwt/JwtRequestFilter.java
+++ src/main/java/com/srp/ServiceBookingSystem/services/jwt/JwtRequestFilter.java
@@ -32,7 +32,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
-
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7); // Remove "Bearer " prefix
             username = jwtUtil.extractUsername(token);
@@ -40,7 +39,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
             if (jwtUtil.validateToken(token, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
